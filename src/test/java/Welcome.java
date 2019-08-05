a8473e7201c40a74af4e030748dff27e31e40852
Mohapatra <Avilash.Mohapatra@thomsonreuters.com>
2019-08-05 19:00:40 +0530

    commit

---
+++ src/test/java/Welcome.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Welcome {
+
+    public static void main(String[] args) {
+
+
+        //Setting webdriver.gecko.driver property
+        System.setProperty("webdriver.chrome.driver", "C:\\TR_Store_Latest\\MyTest\\src\\main\\resources\\chromedriver3.exe");
+
+        //Instantiating driver object and launching browser
+        WebDriver driver=new ChromeDriver();
+
+        //Using get() method to open a webpage
+        driver.get("http://artoftesting.com");
+        driver.get("http://weblogic.com");
+        //Closing the browser
+
+
+    }
+}
